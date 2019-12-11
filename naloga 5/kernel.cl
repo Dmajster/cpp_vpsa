0448f9b44c718aaa25c6849bb80dd78033fec84a
Domen Rostohar <domen.rostohar98@gmail.com>
2019-12-11 18:47:14 +0100

    naloga 5 dela

--- naloga 5/kernel.cl
+++ naloga 5/kernel.cl
@@ -1,15 +1,36 @@
 __kernel void vector_add(
-	__global const int *A,
-	__global const int *B,
-	__global int *C,
-	int size)
+	__global char *image,
+	int width,
+	int height
+)
 {
-	// globalni indeks elementa							
+	const int max_iteration = 800;   //max stevilo iteracij
+	const unsigned char max = 255;   //max vrednost barvnega kanala
+
 	int i = get_global_id(0);
-	// izracun											
-	while (i < size)
+	int j = get_global_id(1);
+
+	const float x0 = (float)j / width * (float)3.5 - (float)2.5; //zacetna vrednost
+	const float y0 = (float)i / height * (float)2.0 - (float)1.0;
+	float x = 0;
+	float y = 0;
+	int iter = 0;
+	//ponavljamo, dokler ne izpolnemo enega izmed pogojev
+	while ((x * x + y * y <= 4) && (iter < max_iteration))
 	{
-		C[i] = A[i] + B[i];
-		i += get_global_size(0);
+		const float xtemp = x * x - y * y + x0;
+		y = 2 * x * y + y0;
+		x = xtemp;
+		iter++;
 	}
+	//izracunamo barvo (magic: http://linas.org/art-gallery/escape/smooth.html)
+	int color = 1.0 + iter - log(log(sqrt(x * x + y * y))) / log(2.0);
+	color = (8 * max * color) / max_iteration;
+	if (color > max)
+		color = max;
+	//zapisemo barvo RGBA (v resnici little endian BGRA)
+	image[4 * i * width + 4 * j + 0] = color; //Blue
+	image[4 * i * width + 4 * j + 1] = color; // Green
+	image[4 * i * width + 4 * j + 2] = color; // Red
+	image[4 * i * width + 4 * j + 3] = 255;   // Alpha
 }
--- naloga 5/main.cpp
+++ naloga 5/main.cpp
@@ -1,128 +1,172 @@
 // "s"cepec preberemo iz datoteke
 #define _CRT_SECURE_NO_WARNINGS
-#include <cstdio>
-#include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
 #include <CL/cl.h>
-#include <memory>
+#include <cmath>
+#include <FreeImage.h>
 
-#define SIZE			(1024)
-#define WORKGROUP_SIZE	(128)
+#define IMAGE_WIDTH	(1024)
+#define IMAGE_HEIGHT (1024)
+#define WORKGROUP_SIZE	(64)
 #define MAX_SOURCE_SIZE	16384
 
 int main()
 {
 	char ch;
 	int i;
+	cl_int ret;
 
-	auto vector_size = SIZE;
+	int imagePixelCount = IMAGE_WIDTH * IMAGE_HEIGHT;
 
+	int image_width = IMAGE_WIDTH;
+	int image_height = IMAGE_HEIGHT;
+
+	// Branje datoteke
+	FILE* fp;
 	char* source_str;
+	size_t source_size;
 
-	const auto fp = fopen("kernel.cl", "r");
+	fp = fopen("kernel.cl", "r");
 	if (!fp)
 	{
 		fprintf(stderr, ":-(#\n");
 		exit(1);
 	}
-	source_str = new char[MAX_SOURCE_SIZE];
-	const auto source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
+	source_str = (char*)malloc(MAX_SOURCE_SIZE);
+	source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
 	source_str[source_size] = '\0';
 	fclose(fp);
 
-	const auto a = new int[vector_size];
-	const auto b = new int[vector_size];
-	const auto c = new int[vector_size];
+	// Rezervacija pomnilnika
+	unsigned char* C = (unsigned char*)malloc(imagePixelCount * sizeof(int));
 
-	for (i = 0; i < vector_size; i++)
-	{
-		a[i] = i;
-		b[i] = vector_size - i;
-	}
-
+	// Podatki o platformi
 	cl_platform_id	platform_id[10];
 	cl_uint			ret_num_platforms;
 	char* buf;
 	size_t			buf_len;
-	auto ret = clGetPlatformIDs(10, platform_id, &ret_num_platforms);
+	ret = clGetPlatformIDs(10, platform_id, &ret_num_platforms);
+	// max. "stevilo platform, kazalec na platforme, dejansko "stevilo platform
 
-	printf("platform id: %d\n", ret);
-	
-	char* profile = nullptr;
-	size_t size;
-	clGetPlatformInfo(platform_id[0], CL_PLATFORM_PROFILE, NULL, profile, &size);
-	profile = static_cast<char*>(malloc(size));
-	clGetPlatformInfo(platform_id[0], CL_PLATFORM_PROFILE, size, profile, nullptr);
-	printf("%s\n", profile);
-	
+	// Podatki o napravi
 	cl_device_id	device_id[10];
 	cl_uint			ret_num_devices;
-	ret = clGetDeviceIDs(platform_id[0], CL_DEVICE_TYPE_GPU, 10, device_id, &ret_num_devices);
-	printf("get 0 device id: %d\n", ret);
+	// Delali bomo s platform_id[0] na GPU
+	ret = clGetDeviceIDs(platform_id[0], CL_DEVICE_TYPE_GPU, 10,
+		device_id, &ret_num_devices);
+	// izbrana platforma, tip naprave, koliko naprav nas zanima
+	// kazalec na naprave, dejansko "stevilo naprav
 
-	
-	const auto context = clCreateContext(nullptr, 1, &device_id[0], nullptr, nullptr, &ret);
-	const auto command_queue = clCreateCommandQueueWithProperties(context, device_id[0], nullptr, &ret);
+	// Kontekst
+	cl_context context = clCreateContext(NULL, 1, &device_id[0], NULL, NULL, &ret);
+	// kontekst: vklju"cene platforme - NULL je privzeta, "stevilo naprav, 
+	// kazalci na naprave, kazalec na call-back funkcijo v primeru napake
+	// dodatni parametri funkcije, "stevilka napake
 
-	size_t local_item_size = WORKGROUP_SIZE;
-	const auto num_groups = (vector_size - 1) / local_item_size + 1;
-	auto global_item_size = num_groups * local_item_size;
+	// Ukazna vrsta
+	cl_command_queue command_queue = clCreateCommandQueueWithProperties(context, device_id[0], 0, &ret);
+	// kontekst, naprava, INORDER/OUTOFORDER, napake
 
-	auto a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, vector_size * sizeof(int), a, &ret);
-	auto b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, vector_size * sizeof(int), b, &ret);
-	auto c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, vector_size * sizeof(int), nullptr, &ret);
+	// Delitev dela
+	size_t local_item_size[2] = { sqrt(WORKGROUP_SIZE), sqrt(WORKGROUP_SIZE) };
+	size_t global_item_size[2] = { IMAGE_WIDTH, IMAGE_HEIGHT };
 
-	const auto program = clCreateProgramWithSource(context, 1, const_cast<const char**>(&source_str), nullptr, &ret);
+	printf("local size: %zu, %zu\n", local_item_size[0], local_item_size[1]);
+	printf("global size: %zu, %zu\n", global_item_size[0], global_item_size[1]);
 
-	ret = clBuildProgram(program, 1, &device_id[0], nullptr, nullptr, nullptr);
-	printf("get build info: %d\n", ret);
-	
+	// Alokacija pomnilnika na napravi
+	cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, imagePixelCount * sizeof(int), NULL, &ret);
+
+	// Priprava programa
+	cl_program program = clCreateProgramWithSource(context, 1, (const char**)&source_str, NULL, &ret);
+	// kontekst, "stevilo kazalcev na kodo, kazalci na kodo,		
+	// stringi so NULL terminated, napaka													
+
+	// Prevajanje
+	ret = clBuildProgram(program, 1, &device_id[0], NULL, NULL, NULL);
+	printf("%d\n", ret);
+
+	// program, "stevilo naprav, lista naprav, opcije pri prevajanju,
+	// kazalec na funkcijo, uporabni"ski argumenti
+
+	// Log
 	size_t build_log_len;
-	ret = clGetProgramBuildInfo(program, device_id[0], CL_PROGRAM_BUILD_LOG, 0, nullptr, &build_log_len);
-	printf("get program build info: %d\n", ret);
-	
-	const auto build_log = static_cast<char*>(malloc(sizeof(char) * (build_log_len + 1)));
-	ret = clGetProgramBuildInfo(program, device_id[0], CL_PROGRAM_BUILD_LOG, build_log_len, build_log, nullptr);
-	printf("get program build info 2: %d\n", ret);
+	char* build_log;
+	ret = clGetProgramBuildInfo(program, device_id[0], CL_PROGRAM_BUILD_LOG, 0, NULL, &build_log_len);
+	printf("%d\n", ret);
+	// program, "naprava, tip izpisa, 
+	// maksimalna dol"zina niza, kazalec na niz, dejanska dol"zina niza
+	build_log = (char*)malloc(sizeof(char) * (build_log_len + 1));
+	ret = clGetProgramBuildInfo(program, device_id[0], CL_PROGRAM_BUILD_LOG, build_log_len, build_log, NULL);
+	printf("%d\n", ret);
 	printf("%s\n", build_log);
 	free(build_log);
 
-	const auto kernel = clCreateKernel(program, "vector_add", &ret);
+	// "s"cepec: priprava objekta
+	cl_kernel kernel = clCreateKernel(program, "vector_add", &ret);
+	// program, ime "s"cepca, napaka
 
 	size_t buf_size_t;
-	clGetKernelWorkGroupInfo(kernel, device_id[0], CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE, sizeof(buf_size_t), &buf_size_t, nullptr);
-	printf("veckratnik niti = %zu", buf_size_t);
+	clGetKernelWorkGroupInfo(kernel, device_id[0], CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE, sizeof(buf_size_t), &buf_size_t, NULL);
+	printf("veckratnik niti = %d", buf_size_t);
 
 	scanf("%c", &ch);
 
-	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), static_cast<void*>(&a_mem_obj));
-	printf("set kernel arg 0: %d\n", ret);
-	
-	ret |= clSetKernelArg(kernel, 1, sizeof(cl_mem), static_cast<void*>(&b_mem_obj));
-	printf("set kernel arg 1: %d\n", ret);
-	
-	ret |= clSetKernelArg(kernel, 2, sizeof(cl_mem), static_cast<void*>(&c_mem_obj));
-	printf("set kernel arg 2: %d\n", ret);
-	
-	ret |= clSetKernelArg(kernel, 3, sizeof(cl_int), static_cast<void*>(&vector_size));
-	printf("set kernel arg 3: %d\n", ret);
-	
-	ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, nullptr, &global_item_size, &local_item_size, 0, nullptr, nullptr);
-	printf("enqueue nd range kernel %d\n", ret);
-	
-	ret = clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0, vector_size * sizeof(int), c, 0, nullptr, nullptr);
-	printf("enqueue read buffer %d\n", ret);
-	
+	// "s"cepec: argumenti
+	ret |= clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&c_mem_obj);
+	printf("%d\n", ret);
+	ret |= clSetKernelArg(kernel, 1, sizeof(cl_int), (void*)&image_width);
+	printf("%d\n", ret);
+	ret |= clSetKernelArg(kernel, 2, sizeof(cl_int), (void*)&image_height);
+	printf("%d\n", ret);
+	// "s"cepec, "stevilka argumenta, velikost podatkov, kazalec na podatke
 
+
+	// "s"cepec: zagon
+	ret = clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL, global_item_size, local_item_size, 0, NULL, NULL);
+	printf("%d\n", ret);
+	// vrsta, "s"cepec, dimenzionalnost, mora biti NULL, 
+	// kazalec na "stevilo vseh niti, kazalec na lokalno "stevilo niti, 
+	// dogodki, ki se morajo zgoditi pred klicem
+
+	// Kopiranje rezultatov
+	ret = clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0, imagePixelCount * sizeof(int), C, 0, NULL, NULL);
+	printf("%d\n", ret);
+	// branje v pomnilnik iz naparave, 0 = offset
+	// zadnji trije - dogodki, ki se morajo zgoditi prej
+
+	// Prikaz rezultatov
+
+	for (auto i = 0; i < imagePixelCount; i++)
+	{
+		//printf("%d\n", C[i]);
+	}
+		
+
+	 // "ci"s"cenje
 	ret = clFlush(command_queue);
 	ret = clFinish(command_queue);
 	ret = clReleaseKernel(kernel);
 	ret = clReleaseProgram(program);
-	ret = clReleaseMemObject(a_mem_obj);
-	ret = clReleaseMemObject(b_mem_obj);
 	ret = clReleaseMemObject(c_mem_obj);
 	ret = clReleaseCommandQueue(command_queue);
 	ret = clReleaseContext(context);
 
+	int pitch = ((32 * image_width + 31) / 32) * 4;
+	
+	FIBITMAP* dst = FreeImage_ConvertFromRawBits(C, image_width, image_height, pitch,
+		32, FI_RGBA_RED_MASK, FI_RGBA_GREEN_MASK, FI_RGBA_BLUE_MASK, TRUE);
+	FreeImage_Save(FIF_PNG, dst, "mandelbrot_gpu.png", 0);
+
+	
+	free(C);
+
+
+
+	
 	return 0;
 }
+
+// kernel
 
