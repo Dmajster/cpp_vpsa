26c750669265bc6d5333077ce294398ed354e8f1
Domen Rostohar <domen.rostohar98@gmail.com>
2020-01-19 14:19:37 +0100

    final done

--- final project/kernel.cl
+++ final project/kernel.cl
@@ -1,23 +1,4 @@
-__kernel void global_histogram(
-	__global unsigned char* input_image,
-	__global unsigned int* output_bins,
-	int width,
-	int height
-)
-{
-	int x = get_global_id(0);
-	int y = get_global_id(1);
-
-	if( y > height || x > width ){
-		return;
-	}
-
-	int value = input_image[y * width + x];
-
-	atomic_add(&output_bins[value], 1);
-}
-
-__kernel void local_histogram(
+__kernel void histogram(
 	__global unsigned char* input_image,
 	__global unsigned int* output_bins,
 	int width,
@@ -29,12 +10,14 @@ __kernel void local_histogram(
 	int x = get_global_id(0);
 	int y = get_global_id(1);
 
+	int x_size = get_global_size(0);
+
 	int workgroup_x = get_local_id(0);
 	int workgroup_y = get_local_id(1);
 
 	if( workgroup_x == 0 && workgroup_y == 0)
 	{
-		for(int i = 0; i < 256; i++ )
+		for(int i = 0;i < 256;i++ )
 		{
 			localBuffer[i] = 0;
 		}
@@ -54,9 +37,99 @@ __kernel void local_histogram(
 
 	if( workgroup_x == 0 && workgroup_y == 0)
 	{
-		for(int i = 0; i < 256; i++ )
+		for(int i = 0;i < 256;i++ )
 		{
 			atomic_add(&output_bins[i], localBuffer[i]);
 		}
 	}
+}
+
+__kernel void cdf(
+	__global unsigned int* g_idata,
+	__global unsigned int* g_odata,
+	int n
+)
+{
+	__local float temp[256];
+
+	int thid = get_local_id(0);
+
+	int offset = 1;
+	temp[2 * thid] = g_idata[2 * thid];     
+	temp[2 * thid + 1] = g_idata[2 * thid + 1];
+	for (int d = n >> 1; d > 0; d >>= 1)
+	{
+		barrier(CLK_GLOBAL_MEM_FENCE);
+
+		if (thid < d)
+		{
+			int ai = offset * (2 * thid + 1) - 1; 
+			int bi = offset * (2 * thid + 2) - 1;             
+			temp[bi] += temp[ai];
+		}         
+		
+		offset *= 2;
+	}
+	if (thid == 0)
+	{
+		temp[n - 1] = 0;
+	}
+
+	for (int d = 1; d < n; d *= 2)    
+	{
+		offset >>= 1;
+
+		barrier(CLK_GLOBAL_MEM_FENCE);
+
+		if (thid < d)
+		{
+			int ai = offset * (2 * thid + 1) - 1;
+			int bi = offset * (2 * thid + 2) - 1;
+			float t = temp[ai];
+			temp[ai] = temp[bi];
+			temp[bi] += t;
+		}
+	}
+
+	barrier(CLK_GLOBAL_MEM_FENCE);
+
+	g_odata[2 * thid] = temp[2 * thid];
+	g_odata[2 * thid + 1] = temp[2 * thid + 1];
+}
+
+
+
+unsigned int findMin(__global unsigned int* histogram)
+{
+	unsigned int min = 0;
+
+	for (int i = 0; min == 0 && i < 256; i++)
+	{
+		min = histogram[i];
+	}
+
+	return min;
+}
+
+unsigned char scale(unsigned int cdf, unsigned int cdfmin, unsigned int imageSize)
+{
+	float scale = (float)(cdf - cdfmin) / (float)(imageSize - cdfmin);
+	scale = round(scale * (float)(256.0 - 1));
+	return (char)scale;
+}
+
+__kernel void equalize(
+	__global unsigned int* cdf,
+	__global unsigned char* image,
+	unsigned int width,
+	unsigned int heigth
+)
+{
+	unsigned int imageSize = width * heigth;
+	unsigned int cdfmin = findMin(cdf);
+
+	int x = get_global_id(0);
+	int y = get_global_id(1);
+
+	image[y * width + x] = scale(cdf[image[y*width + x]], cdfmin , imageSize);
 }
--- final project/main.cpp
+++ final project/main.cpp
@@ -1,4 +1,3 @@
-// "s"cepec preberemo iz datoteke
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,29 +9,12 @@
 #define MAX_SOURCE_SIZE	16384
 #define BINS 256
 
-void HistogramCPU(unsigned char* imageIn, unsigned int* histogram, int width, int height)
-{
-	memset(histogram, 0, BINS * sizeof(unsigned int));
-
-	//za vsak piksel v sliki
-	for (auto i = 0; i < (height); i++)
-		for (auto j = 0; j < (width); j++)
-		{
-			histogram[imageIn[i * width + j]]++;
-		}
-}
-
-void printHistogram(unsigned int* histogram) {
-	printf("Barva\tPojavitve\n");
-	for (auto i = 0; i < BINS; i++) {
-		printf("%d\t%d\n", i, histogram[i]);
-	}
-}
+#define GRAYLEVELS 256
 
 struct ImageData
 {
-	unsigned int image_width;
-	unsigned int image_height;
+	unsigned int width;
+	unsigned int height;
 	unsigned char* pixels;
 };
 
@@ -50,42 +32,52 @@ ImageData* load_image(const char* file_name)
 		new unsigned char[image_width * image_height]
 	};
 
-	FreeImage_ConvertToRawBits(image_data->pixels, image_bitmap_grey, image_data->image_width, 8, 0xFF, 0xFF, 0xFF, TRUE);
+	FreeImage_ConvertToRawBits(image_data->pixels, image_bitmap_grey, image_data->width, 8, 0xFF, 0xFF, 0xFF, TRUE);
 	FreeImage_Unload(image_bitmap_grey);
 	FreeImage_Unload(image_bitmap);
 
 	return image_data;
 }
 
+char* load_kernel_source(const char* file_name)
+{
+	const auto file = fopen(file_name, "r");
+	if (!file)
+	{
+		fprintf(stderr, "Failed to open kernel file: %s \n", file_name);
+		exit(1);
+	}
 
+	const auto source_string = static_cast<char*>(malloc(MAX_SOURCE_SIZE));
+	const auto source_size = fread(source_string, 1, MAX_SOURCE_SIZE, file);
+	source_string[source_size] = '\0';
+	fclose(file);
+
+	return source_string;
+}
+
+void printHistogram(unsigned int* histogram) {
+	printf("Barva\tPojavitve\n");
+	for (int i = 0; i < BINS; i++) {
+		printf("%d\t%d\n", i, histogram[i]);
+	}
+}
 
 int main()
 {
-	auto image_information = load_image("slika.png");
-	
+	auto image = load_image("slika.png");
+	auto kernel_source = load_kernel_source("kernel.cl");
+	auto gray_levels = GRAYLEVELS;
 
-	const auto bins = static_cast<unsigned int*>(calloc(BINS * sizeof(unsigned int), sizeof(unsigned int)));
-	
-	char* source_str;
-	const auto fp = fopen("kernel.cl", "r");
-	if (!fp)
-	{
-		fprintf(stderr, ":-(#\n");
-		exit(1);
-	}
-	source_str = static_cast<char*>(malloc(MAX_SOURCE_SIZE));
-	const auto source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
-	source_str[source_size] = '\0';
-	fclose(fp);
+	auto bins = (unsigned int*)calloc(GRAYLEVELS, sizeof(unsigned int));
 
 	cl_platform_id	platform_id[10];
 	cl_uint			ret_num_platforms;
-	char* buf;
 	auto ret = clGetPlatformIDs(10, platform_id, &ret_num_platforms);
 
 	cl_device_id	device_id[10];
 	cl_uint			ret_num_devices;
-	ret = clGetDeviceIDs(platform_id[0], CL_DEVICE_TYPE_GPU, 10, device_id, &ret_num_devices);
+	ret = clGetDeviceIDs(platform_id[0], CL_DEVICE_TYPE_CPU, 10, device_id, &ret_num_devices);
 
 	const auto context = clCreateContext(nullptr, 1, &device_id[0], nullptr, nullptr, &ret);
 
@@ -94,71 +86,78 @@ int main()
 	const size_t WORKGROUP_SIDE_LENGTH = sqrt(WORKGROUP_SIZE);
 
 	size_t local_item_size[2] = { WORKGROUP_SIDE_LENGTH, WORKGROUP_SIDE_LENGTH };
-	size_t global_item_size[2] = { ceil(image_information->image_width / static_cast<float>(local_item_size[0])) * local_item_size[0], ceil(image_information->image_height / static_cast<float>(local_item_size[1])) * local_item_size[1] };
+	size_t global_item_size[2] = { ceil(image->width / static_cast<float>(local_item_size[0]))* local_item_size[0], ceil(image->height / static_cast<float>(local_item_size[1]))* local_item_size[1] };
 
-	printf("local size: %zu, %zu\n", local_item_size[0], local_item_size[1]);
-	printf("global size: %zu, %zu\n", global_item_size[0], global_item_size[1]);
+	auto image_input_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, image->width * image->height * sizeof(unsigned char), image->pixels, &ret);
+	auto histogram_mem_obj = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, BINS * sizeof(unsigned int*), bins, &ret);
+	auto cdf_histogram_mem_obj = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, BINS * sizeof(unsigned int*), bins, &ret);
 
-	auto image_input_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, image_information->image_width * image_information->image_height * sizeof(char), image_information->pixels, &ret);
-	auto bins_output_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY | CL_MEM_COPY_HOST_PTR, BINS * sizeof(unsigned int), bins, &ret);
-
-	const auto program = clCreateProgramWithSource(context, 1, const_cast<const char**>(&source_str), nullptr, &ret);
+	const auto program = clCreateProgramWithSource(context, 1, const_cast<const char**>(&kernel_source), nullptr, &ret);
 
 	ret = clBuildProgram(program, 1, &device_id[0], nullptr, nullptr, nullptr);
-	printf("%d\n", ret);
 
 	size_t build_log_len;
 	ret = clGetProgramBuildInfo(program, device_id[0], CL_PROGRAM_BUILD_LOG, 0, nullptr, &build_log_len);
-	printf("%d\n", ret);
 
-	const auto build_log = static_cast<char*>(malloc(sizeof(char) * (build_log_len + 1)));
+	const auto build_log = static_cast<char*>(malloc(sizeof(char)* (build_log_len + 1)));
 	ret = clGetProgramBuildInfo(program, device_id[0], CL_PROGRAM_BUILD_LOG, build_log_len, build_log, nullptr);
-	printf("%d\n", ret);
 	printf("%s\n", build_log);
 	free(build_log);
 
-	const auto kernel = clCreateKernel(program, "local_histogram", &ret);
+	const auto histogram_kernel = clCreateKernel(program, "histogram", &ret);
 
 	size_t buf_size_t;
-	clGetKernelWorkGroupInfo(kernel, device_id[0], CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE, sizeof(buf_size_t), &buf_size_t, nullptr);
-	printf("veckratnik niti = %d", buf_size_t);
+	clGetKernelWorkGroupInfo(histogram_kernel, device_id[0], CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE, sizeof(buf_size_t), &buf_size_t, nullptr);
+	ret |= clSetKernelArg(histogram_kernel, 0, sizeof(cl_mem), static_cast<void*>(&image_input_mem_obj));
+	ret |= clSetKernelArg(histogram_kernel, 1, sizeof(cl_mem), static_cast<void*>(&histogram_mem_obj));
+	ret |= clSetKernelArg(histogram_kernel, 2, sizeof(cl_int), static_cast<void*>(&image->width));
+	ret |= clSetKernelArg(histogram_kernel, 3, sizeof(cl_int), static_cast<void*>(&image->height));
 
-	ret |= clSetKernelArg(kernel, 0, sizeof(cl_mem), static_cast<void*>(&image_input_mem_obj));
-	printf("%d\n", ret);
+	const auto cdf_kernel = clCreateKernel(program, "cdf", &ret);
+	clGetKernelWorkGroupInfo(cdf_kernel, device_id[0], CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE, sizeof(buf_size_t), &buf_size_t, nullptr);
+	ret |= clSetKernelArg(cdf_kernel, 0, sizeof(cl_mem), static_cast<void*>(&histogram_mem_obj));
+	ret |= clSetKernelArg(cdf_kernel, 1, sizeof(cl_mem), static_cast<void*>(&cdf_histogram_mem_obj));
+	ret |= clSetKernelArg(cdf_kernel, 2, sizeof(cl_int), static_cast<void*>(&gray_levels));
 
-	ret |= clSetKernelArg(kernel, 1, sizeof(cl_mem), static_cast<void*>(&bins_output_mem_obj));
-	printf("%d\n", ret);
-
-	ret |= clSetKernelArg(kernel, 2, sizeof(cl_int), static_cast<void*>(&image_information->image_width));
-	printf("%d\n", ret);
-	ret |= clSetKernelArg(kernel, 3, sizeof(cl_int), static_cast<void*>(&image_information->image_height));
-	printf("%d\n", ret);
-
-	ret = clEnqueueWriteBuffer(command_queue, image_input_mem_obj, CL_TRUE, 0, image_information->image_width * image_information->image_height * sizeof(char), image_information->pixels, 0, nullptr, nullptr);
-
-	ret = clEnqueueNDRangeKernel(command_queue, kernel, 2, nullptr, global_item_size, local_item_size, 0, nullptr, nullptr);
-	printf("%d\n", ret);
+	const auto eq_kernel = clCreateKernel(program, "equalize", &ret);
+	clGetKernelWorkGroupInfo(eq_kernel, device_id[0], CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE, sizeof(buf_size_t), &buf_size_t, nullptr);
+	ret |= clSetKernelArg(eq_kernel, 0, sizeof(cl_mem), static_cast<void*>(&cdf_histogram_mem_obj));
+	ret |= clSetKernelArg(eq_kernel, 1, sizeof(cl_mem), static_cast<void*>(&image_input_mem_obj));
+	ret |= clSetKernelArg(eq_kernel, 2, sizeof(cl_int), static_cast<void*>(&image->width));
+	ret |= clSetKernelArg(eq_kernel, 3, sizeof(cl_int), static_cast<void*>(&image->height));
 
 
-	ret = clEnqueueReadBuffer(command_queue, bins_output_mem_obj, CL_TRUE, 0, BINS * sizeof(unsigned int), bins, 0, nullptr, nullptr);
-	printf("%d\n", ret);
+	ret = clEnqueueWriteBuffer(command_queue, image_input_mem_obj, CL_TRUE, 0, image->width * image->height * sizeof(char), image->pixels, 0, nullptr, nullptr);
+	cl_event histogram_event;
+	ret = clEnqueueNDRangeKernel(command_queue, histogram_kernel, 2, nullptr, global_item_size, local_item_size, 0, nullptr, &histogram_event);
 
-	//HistogramCPU(image_input, bins, image_width, image_height);
-
-	printHistogram(bins);
-
+	cl_event cdf_event;
+	size_t cdf_global_size = 128;
+	size_t cdf_local_size = 128;
+	ret = clEnqueueNDRangeKernel(command_queue, cdf_kernel, 1, nullptr, &cdf_global_size, &cdf_local_size, 1, &histogram_event, &cdf_event);
+	ret = clEnqueueReadBuffer(command_queue, histogram_mem_obj, CL_TRUE, 0, 256 * sizeof(unsigned int), bins, 0, nullptr, nullptr);
+	ret = clEnqueueNDRangeKernel(command_queue, eq_kernel, 2, nullptr, global_item_size, local_item_size, 1, &cdf_event, nullptr);
+	ret = clEnqueueReadBuffer(command_queue, cdf_histogram_mem_obj, CL_TRUE, 0, 256 * sizeof(unsigned int), bins, 0, nullptr, nullptr);
+	ret = clEnqueueReadBuffer(command_queue, image_input_mem_obj, CL_TRUE, 0, image->width * image->height * sizeof(char), image->pixels, 0, nullptr, nullptr);
 	ret = clFlush(command_queue);
 	ret = clFinish(command_queue);
-	ret = clReleaseKernel(kernel);
+	ret = clReleaseKernel(histogram_kernel);
 	ret = clReleaseProgram(program);
 	ret = clReleaseMemObject(image_input_mem_obj);
-	ret = clReleaseMemObject(bins_output_mem_obj);
+	ret = clReleaseMemObject(histogram_mem_obj);
+	ret = clReleaseMemObject(cdf_histogram_mem_obj);
 	ret = clReleaseCommandQueue(command_queue);
 	ret = clReleaseContext(context);
 
-	free(image_information->pixels);
-	free(image_information);
+	const auto imageOutBitmap = FreeImage_ConvertFromRawBits(image->pixels, image->width, image->height, image->width, 8, 0xFF, 0xFF, 0xFF, TRUE);
+	FreeImage_Save(FIF_PNG, imageOutBitmap, "image_out.png", 0);
+	FreeImage_Unload(imageOutBitmap);
+
+	free(image->pixels);
+	free(image);
 
 	return 0;
 }
+
+
 
